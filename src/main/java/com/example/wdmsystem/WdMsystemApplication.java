b7174177b199250a5f75f69c3e63a58a70a107dd
MARTO <martynasled@gmail.com>
2024-11-03 20:04:31 +0200

    Initial commit

---
+++ src/main/java/com/example/wdmsystem/WdMsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.wdmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdMsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WdMsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wdmsystem/WdMsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wdmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdMsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
