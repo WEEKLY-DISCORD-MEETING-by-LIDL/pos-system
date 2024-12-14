3255e9c32c01984e3a48f4de2c39692ec97e7501
AkvileR <56289629+AkvileR@users.noreply.github.com>
2024-12-14 18:02:36 +0200

    Home page

--- pos-system-front/src/components/Home.js
+++ pos-system-front/src/components/Home.js
@@ -1,38 +1,27 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from "react-router-dom";
+import "../styles/HomeStyle.css";
 
 const Home = () => {
-    const [text, setText] = useState([]);
-    const [axiosText, setAxiosText] = useState([])
     const navigate = useNavigate();
-
-    // useEffect(() => {
-    //     fetch('http://localhost:8080/test', {
-    //         method: 'GET',
-    //         credentials: 'include',
-    //     })
-    //         .then(response => response.text())
-    //         .then(data => setText(data))
-    //         .catch(error => console.error("Error doing something", error));
-    //
-    //     axios.get(`http://localhost:8080/test2`)
-    //         .then(res => {
-    //           const axiosText = res.data;
-    //           setAxiosText(axiosText);
-    //         })
-    // }, []);
 
     const returnBack = () => {
         navigate("/")
-      }
+    }
+
+    const goToCreateOrder = () => {
+        navigate("/order")
+    }
 
     return (
-        <div>
-            <h1>Users</h1>
-            <div>fetch: {text}</div>
-            <div>axios: {axiosText}</div>
-            <button onClick={returnBack}>Return</button>
+        <div className="home-container">
+            <h1>Home/Navigation Page</h1>
+
+            <div className="button-container">
+                <button onClick={goToCreateOrder}>Orders</button>
+
+                <button onClick={returnBack}>Back to Login</button>
+            </div>
         </div>
     )
 }
