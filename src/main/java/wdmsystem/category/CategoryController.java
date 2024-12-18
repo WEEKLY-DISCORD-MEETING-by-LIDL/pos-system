e999fac646165de7db1ea6d3d94b3d090800fac5
MARTO <martynasled@gmail.com>
2024-12-18 19:18:58 +0200

    login logging

--- src/main/java/wdmsystem/category/CategoryController.java
+++ src/main/java/wdmsystem/category/CategoryController.java
@@ -2,6 +2,7 @@ package wdmsystem.category;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class CategoryController {
 
@@ -25,28 +27,36 @@ public class CategoryController {
     @PostMapping("/categories")
     @PreAuthorize("hasRole('ADMIN') or hasRole('OWNER')")
     ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO request) {
+        log.info("Received request to create category: {}", request);
         CategoryDTO newCategory = _categoryService.createCategory(request);
+        log.info("Category created successfully: {}", newCategory);
         return new ResponseEntity<>(newCategory, HttpStatus.CREATED);
     }
 
     @GetMapping("/categories")
     @PreAuthorize("hasRole('ADMIN') or hasRole('OWNER') or hasRole('REGULAR')")
     ResponseEntity<List<CategoryDTO>> getCategories() {
+        log.info("Fetching all categories.");
         List<CategoryDTO> categoryList = _categoryService.getCategories();
+        log.info("Retrieved {} categories.", categoryList.size());
         return new ResponseEntity<>(categoryList, HttpStatus.OK);
     }
 
     @PutMapping("/categories/{categoryId}")
     @PreAuthorize("hasRole('ADMIN') or (hasRole('OWNER') and @categoryService.isOwnedByCurrentUser(#categoryId))")
     ResponseEntity<CategoryDTO> updateCategory(@PathVariable int categoryId, @RequestBody CategoryDTO request) {
+        log.info("Received request to update category with ID: {}", categoryId);
         CategoryDTO updatedCategory = _categoryService.updateCategory(categoryId, request);
+        log.info("Category with ID: {} updated successfully", categoryId);
         return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
     }
 
     @DeleteMapping("/categories/{categoryId}")
     @PreAuthorize("hasRole('ADMIN') or (hasRole('OWNER') and @categoryService.isOwnedByCurrentUser(#categoryId))")
     ResponseEntity<Category> deleteCategory(@PathVariable int categoryId) {
+        log.info("Received request to delete category with ID: {}", categoryId);
         _categoryService.deleteCategory(categoryId);
+        log.info("Category with ID: {} deleted successfully", categoryId);
         return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
